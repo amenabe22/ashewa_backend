6551b0286b6cf7b1c1080eed910e3a1db1c66edd
amenabe22 <bdere12345@gmail.com>
2021-01-23 06:13:46 -0500

    final backend update

--- core_ecommerce/admin.py
+++ core_ecommerce/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from .models import (ParentCategory, Category, SubCategory,
                      Products, ProductImage, LandingCarousel)
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'selling_price','vendor', 'product_parent_category','product_brand','stock_amount',)
+
 admin.site.register(ParentCategory)
 admin.site.register(Category)
 admin.site.register(SubCategory)
-admin.site.register(Products)
+admin.site.register(Products, ProductAdmin)
 admin.site.register(ProductImage)
 admin.site.register(LandingCarousel)
