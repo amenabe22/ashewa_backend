61cf9eaabd56e6c0c3d895d021022d485ecc8e9c
root <root@serv.ashewa.com>
2021-02-06 20:45:07 +0000

    importatn production changes

--- ashewa_final/settings.py
+++ ashewa_final/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bz@sgxdckq_441i-gr=ms%*7x!+x&pgua37!jfm@&2bi$b4mr5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -186,6 +186,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+#CORS_ORIGIN_ALLOW_ALL = True
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+AUTH_USER_MODEL = 'accounts.CustomUser'
+#STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
