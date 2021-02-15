ccdffa1409d2fc55621f56646a601ef68f5e2a79
amenabe22 <bdere12345@gmail.com>
2021-02-15 15:18:30 +0300

    store subdomain setup

--- ashewa_final/settings.py
+++ ashewa_final/settings.py
@@ -49,8 +49,8 @@ INSTALLED_APPS += [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django_hosts.middleware.HostsRequestMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_hosts.middleware.HostsResponseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'ashewa_final.urls'
